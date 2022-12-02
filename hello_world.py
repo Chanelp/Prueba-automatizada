19d1f516951a6fa9fa03967ed586a6b97a929298
Chanelp <mariannissanchez27@gmail.com>
2022-12-02 19:30:20 -0400

    Encontrando elementos con find_element

--- hello_world.py
+++ hello_world.py
@@ -4,26 +4,41 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 
-
 class HolaMundo(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls) -> None:
+    def setUp(self) -> None:
         chrome = Service(r"C:\Users\ADMIN\Desktop\selenium\chromedriver.exe")
-        cls.driver = webdriver.Chrome(service=chrome)
-        WebDriverWait(cls.driver, 10)
 
-    def test_visitar_wikipedia(self):
-        self.driver.get('https://www.wikipedia.org')
+        self.driver = webdriver.Chrome(service= chrome)
+        driver = self.driver
+        driver.get('http://demo-store.seleniumacademy.com/')
+        driver.maximize_window()
 
-    def test_hola(self):
-        self.driver.get("https://www.google.com/")
+    def test_search_text_field(self):
+        search_field = self.driver.find_element("id", "search")
 
-    @classmethod
-    def tearDownClass(cls) -> None:
-        cls.driver.quit()
+    def test_search_text_field_by_name(self):
+        search_field = self.driver.find_element("name", "q")
 
+    def test_search_field_by_class_name(self):
+        self_field = self.driver.find_element("class name", "input-text")
+
+    def test_search_button_enabled(self):
+        button = self.driver.find_element("class name", "button")
+
+    def test_count_of_promo_banner_images(self):
+        banner_list = self.driver.find_element("class name", "promos")
+        banners = banner_list.find_elements("tag name", "img")
+        self.assertEqual(3, len(banners))
+    
+    def test_vip_promo(self):
+        vip_promo = self.driver.find_elements('xpath', '//*[@id="top"]/body/div/div[2]/div[2]/div/div/div[2]/div[1]/ul/li[3]/a/img')
+
+    def test_shopping_cart(self):
+        shopping_cart_icon = self.driver.find_elements("css selector", "div.header-minicart span.icon")
+
+    def tearDown(self) -> None:
+        self.driver.quit()
 
 if __name__ == "__main__":
-    unittest.main(verbosity=2, testRunner=HTMLTestRunner(
-        output="report", report_name="holareport"))
+    unittest.main(verbosity= 2)
